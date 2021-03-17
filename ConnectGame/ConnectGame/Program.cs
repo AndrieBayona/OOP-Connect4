17210f00c5d20ce39b8a2e48f53c557544157ebc
Lyndon-Bualat <64567818+Lyndon-Bualat@users.noreply.github.com>
2021-03-17 14:30:24 -0600

    Update Program.cs

--- ConnectGame/ConnectGame/Program.cs
+++ ConnectGame/ConnectGame/Program.cs
@@ -6,6 +6,83 @@ using System.Threading.Tasks;
 
 namespace ConnectGame
 {
+    public class Board // Updated Lyndon
+    {
+
+        //add method to check 'bottom'
+        //add method to display and update board
+        //check winning combination
+        // restart game
+
+       
+
+         public Board() {
+
+            
+         }
+       
+
+        public static void BoardDisplay(string[,] board)
+        {
+          Console.WriteLine("Connect 4 Game Development Project: \n");
+
+            int i = 1;
+            while (i <= 6)
+            {
+                Console.Write("|");
+                int x = 1;
+                while (x <= 7)
+                {
+                   
+                        board[i, x] = " X ";
+                        Console.Write(board[i, x]);
+                             
+                    x++;
+                }
+                i++;
+                Console.Write("| \n");
+            }
+            Console.Write(" "+" 1 " + " 2 " + " 3 " + " 4 " + " 5 " + " 6 " + " 7 ");
+        }
+
+
+        
+        public static void RestartBoard(string[,] board)
+        {
+            Console.WriteLine("Restart? Yes(1) No(0):");
+            string num = Console.ReadLine();
+            if (num == "1")
+            {
+                int i = 1;
+                Console.WriteLine(" ");
+                while (i <= 6)
+                {
+                    Console.Write("|");
+                    int x = 1;
+                    while (x <= 7)
+                    {
+                        board[i, x] = " # ";
+                        Console.Write(board[i, x]);                       
+                        x++;
+                    }
+                    i++;
+                    Console.Write("| \n");
+                }
+                Console.Write(" " + " 1 " + " 2 " + " 3 " + " 4 " + " 5 " + " 6 " + " 7 ");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for playing!");
+            }
+           
+        }
+
+        //public static bool WinnerBoard()
+        //{
+
+        //}
+    }
+    
     class Program
     {
         static void Main(string[] args)
