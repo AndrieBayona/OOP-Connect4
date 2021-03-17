61a516c79cfa77ccde124dffb28fa6d50773305c
AndrieBayona <73300719+AndrieBayona@users.noreply.github.com>
2021-03-17 14:37:43 -0600

    Update Program.cs

--- ConnectGame/ConnectGame/Program.cs
+++ ConnectGame/ConnectGame/Program.cs
@@ -6,6 +6,64 @@ using System.Threading.Tasks;
 
 namespace ConnectGame
 {
+    //skeleton code at its current state
+    
+    public class Player
+    {
+
+        public string Name { get; set; }
+        public char Piece { get; set; }
+        public Player (string name, char piece)
+        {
+            Name = name;
+            Piece = piece;
+        }
+
+        public virtual void testInfo()
+        {
+            Console.WriteLine("Hello {0}", Name);
+        }
+        //checks turn
+        //display winner
+        //display turns
+
+    }
+
+    class Player1 : Player
+    {
+        //will inherit from player
+        public Player1(string name, char piece) : base(name, piece)
+        {
+
+        }
+
+        public override void testInfo()
+        {
+            Console.WriteLine("Hello {0}, you'll be '{1}'", Name, Piece);
+        }
+
+
+    }
+
+    class Player2 : Player
+    {
+        //will inherit from player
+        public Player2(string name, char piece) : base(name, piece)
+        {
+
+        }
+
+        public override void testInfo()
+        {
+            Console.WriteLine("Hello {0}, you'll be '{1}'", Name, Piece);
+        }
+    }
+    
+    
+    
+    
+    
+    
     public class Board // Updated Lyndon
     {
 
@@ -87,6 +145,65 @@ namespace ConnectGame
     {
         static void Main(string[] args)
         {
+            
+            // the main
+
+            ////initiating players
+            string[,] board = new string[7, 8]; //length is 52
+            string p1, p2;
+            int reset = 0;
+            Console.WriteLine("Enter player information: ");
+            Console.Write("Player 1: ");
+            p1 = Console.ReadLine();
+            Console.Write("Player 2: ");
+            p2 = Console.ReadLine();
+
+            Player player1 = new Player1(p1, 'X');
+            Player player2 = new Player2(p2, '0');
+
+            player1.testInfo();
+            player2.testInfo();
+
+            Player obj = player1;
+            Player obj2 = player2;
+
+            //gameplay loop
+
+            //display board
+            Console.WriteLine("Game Start");
+            do
+            {
+                Controller.PlayerTurn(obj);
+                Controller.PlacePiece(obj.Piece);
+                //display board
+                //method to check bottom/win
+                //if condition to display when player 1 wins 
+                //display winner and ask user if they want to play again.
+
+                Controller.PlayerTurn(obj2);
+                Controller.PlacePiece(obj2.Piece);
+                //display board
+                //method to check bottom/win
+                //if condition to display when player 1 wins 
+                //display winner and ask user if they want to play again.
+
+
+                //for testing and debugging
+                Console.WriteLine("Reset?");
+                Board.RestartBoard(board);
+
+
+
+            } while ();
+
+
+            Board.BoardDisplay(board);
+            Console.WriteLine(" ");
+            
+
+
+
+            Console.Read();
 
           
         }
